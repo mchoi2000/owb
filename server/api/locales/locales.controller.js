3aa8e9db6ffc249fa135d73957c933e0be87e923
PRADEEP KADIYALA <pkadiya@us.ibm.com>
2016-11-11 17:46:00 -0600

    Updated the i18-suppport to be a local json file for now

--- server/api/locales/locales.controller.js
+++ server/api/locales/locales.controller.js
@@ -6,8 +6,11 @@
 //Schedule Contract with IBM Corp.
 'use strict';
 var _ = require('lodash');
-var i18nSupport = require('../../../node_modules/@marketplace/i18n-support');
+var fs = require('fs');
+var path = require('path');
 var sevenseas = require('../../../node_modules/@marketplace/sevenseas');
+var i18nSupport = JSON.parse(
+  fs.readFileSync(path.normalize(__dirname + '/../../config/i18n-support.json')));
 
 module.exports = {
   getCountriesWithLang: function(req, res) {
