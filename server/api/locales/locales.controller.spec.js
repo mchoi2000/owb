5936b44da9960c5be6cbf1c5da86b49547087398
Vasantha Vurakaranam <vasantha@vasanthas-mbp.austin.ibm.com>
2016-11-18 12:47:34 -0600

    Adding sevenseas as a node_module

--- server/api/locales/locales.controller.spec.js
+++ server/api/locales/locales.controller.spec.js
@@ -24,7 +24,7 @@ describe('country controller', function() {
 
   it('fail when sevenseas locale not loaded', function() {
     var _countryController = proxyquire('./locales.controller', {
-      '../../sevenseas' : {
+      '../../../node_modules/@marketplace/sevenseas' : {
         getWCMLanguageAndViewMap : function() {
           return;
         }
